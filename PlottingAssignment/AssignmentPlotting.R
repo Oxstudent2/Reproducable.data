f0c64fa06ac00bac42a3440511e18bed7b799dc7
XYZ248 <150162434+Oxstudent2@users.noreply.github.com>
2023-12-06 16:43:39 +0000

    Update AssignmentPlotting.R
    edit and fixed code

--- PlottingAssignment/AssignmentPlotting.R
+++ PlottingAssignment/AssignmentPlotting.R
@@ -48,6 +48,29 @@ normal_plot_check <- function(penguins_clean) {
        title = 'Checking for normal distrobution')
 }
 
+save_penguin2_plot_svg <- function(penguins_clean, 
+                                  AssignmentFigures, size, res, scaling)
+{
+  svglite("AssignmentFigures/CompPenguin2.svg", 
+          width   =  5.9, 
+          height  =  5.9)
+# CompFigAll <- plot_comp_data_all(penguins_clean)
+  print(CompFigAll)
+  dev.off()
+}
+
+save_penguin2_plot_png <- function(penguins_clean, AssignmentFigures, size, res, scaling)
+{
+  agg_png("AssignmentFigures/CompPenguin2.png", 
+          width = 20, 
+          height = 15, 
+          units = "cm", 
+          res = 600, 
+          scaling = 1)
+ # CompFigAll <- plot_comp_data_all(penguins_clean)
+  print(CompFigAll)
+  dev.off()
+}
 
 save_penguin_plot_svg <- function(penguins_clean, 
                                   AssignmentFigures, size, res, scaling)
@@ -55,7 +78,7 @@ save_penguin_plot_svg <- function(penguins_clean,
   svglite("AssignmentFigures/CompVector.svg", 
           width   =  5.9, 
           height  =  5.9)
-  # CompFig <- plot_comp_data(penguins_clean)
+ #CompFig <- plot_comp_data(penguins_clean)
   print(CompFig)
   dev.off()
 }
@@ -69,26 +92,105 @@ save_penguin_plot_png <- function(penguins_clean, AssignmentFigures, size, res, 
          units = "cm", 
          res = 600, 
          scaling = 1)
-   #CompFig <- plot_comp_data(penguins_clean)
+ #  CompFig <- plot_comp_data(penguins_clean)
   print(CompFig)
   dev.off()
 }
-  
-# code was saved bad, open R from folder asn shut things down again - check file paths - 
-# thin rest of it was okay
 
- #save_penguin_plot_svg <- function(penguins_clean)
-#{
- # svglite("AssignmentFigures/fig01_vector.svg", 
-  #       width = 5.9, 
-   #      height = 5.9)
-#  CompDataPlot <- plot_comp_data(penguins_clean)
- # CompDataPlot_DF <- as.data.frame(CompDataPlot)
-  #print(CompDataPlot_DF)
-  #dev.off()
-  
- # }
-  
-  
+
+save_gentoo_plot_png <- function(penguins_clean, AssignmentFigures, size, res, scaling)
+{
+  agg_png("AssignmentFigures/GentooFigureExport.png", 
+          width = 20, 
+          height = 15, 
+          units = "cm", 
+          res = 600, 
+          scaling = 1)
+  GentooFigure <- GentooCorFig(GentooOnly)
+  print(GentooFigure)
+  dev.off()
+}
+
+save_chinstrap_plot_png <- function(penguins_clean, AssignmentFigures, size, res, scaling)
+{
+  agg_png("AssignmentFigures/ChinstrapFigureExport.png", 
+          width = 20, 
+          height = 15, 
+          units = "cm", 
+          res = 600, 
+          scaling = 1)
+  ChinstrapFigure <- ChinstrapCorFig(ChinstrapOnly)
+  print(ChinstrapFigure)
+  dev.off()
+}
+
+save_adelie_plot_png <- function(penguins_clean, AssignmentFigures, size, res, scaling)
+{
+  agg_png("AssignmentFigures/AdelieFigureExport.png", 
+          width = 20, 
+          height = 15, 
+          units = "cm", 
+          res = 600, 
+          scaling = 1)
+  AdelieFigure <- AdelieCorFig(AdelieOnly)
+  print(AdelieFigure)
+  dev.off()
+}
+
+save_all_species_plot_png <- function(penguins_clean)
+{
+  agg_png("AssignmentFigures/AllSpeciesPlot.png", 
+          width = 20, 
+          height = 15, 
+          units = "cm", 
+          res = 600, 
+          scaling = 1)
+#  AllSpeciesFigure <- PenguinsCorFig(penguins_clean)
+  print(AllSpeciesFigure)
+  dev.off()
+}
+
+save_chinstrap_plot_svg <- function(ChinstrapOnly, 
+                                  AssignmentFigures, size, res, scaling)
+{
+  svglite("AssignmentFigures/ChinstrapVector.svg", 
+          width   =  5.9, 
+          height  =  5.9)
+ # ChinstrapFigure<- ChinstrapCorFig(ChinstrapOnly)
+  print(ChinstrapFigure)
+  dev.off()
+}
+
+save_adelie_plot_svg <- function(penguins_clean, 
+                                    AssignmentFigures, size, res, scaling)
+{
+  svglite("AssignmentFigures/AdelieVector.svg", 
+          width   =  5.9, 
+          height  =  5.9)
+  AdelieFigure <- AdelieCorFig(AdelieOnly)
+  print(AdelieFigure)
+  dev.off()
+}
+
+save_gentoo_plot_svg <- function(penguins_clean, 
+                                 AssignmentFigures, size, res, scaling)
+{
+  svglite("AssignmentFigures/GentooVector.svg", 
+          width   =  5.9, 
+          height  =  5.9)
+  GentooFigure <- GentooCorFig(GentooOnly)
+  print(GentooFigure)
+  dev.off()
+}
+
+save_all_species_plot_svg <- function(penguins_clean)
+{
+  svglite("AssignmentFigures/AllSpeciesVector.svg", 
+          width   =  5.9, 
+          height  =  5.9)
+ # AllSpeciesFigure <- PenguinsCorFig(penguins_clean)
+  print(AllSpeciesFigure)
+  dev.off()
+}
 
 
